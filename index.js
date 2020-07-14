7d119400d3bdb842a4e87502f130d5ab50ef8dd5
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-14 19:49:05 +0700

    adding response[0]

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.post("/webhook", (req, res) => {
     console.log("This is quantity", quantity);
     console.log("This is type", type);
     console.log(agent.fulfillmentText);
-    console.log(agent.consoleMessages.Text.text);
-    agent.add(agent.consoleMessages.Text.text);
+    console.log(agent.consoleMessages[0].Text.text);
+    agent.add(agent.consoleMessages[0].Text.text);
   };
   const testhook = (agent) => {
     agent.add("The webhook is working!");

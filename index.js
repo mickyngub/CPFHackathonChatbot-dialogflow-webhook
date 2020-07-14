be35d5c8a1d40604939ed84e17657659b455cc0d
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-14 12:19:22 +0700

    change intent's name

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/webhook", (req, res) => {
   }
 
   let intentMap = new Map();
-  intentMap.set("Testhook", testhook);
+  intentMap.set("besthook", testhook);
   agent.handleRequest(intentMap);
 });
 

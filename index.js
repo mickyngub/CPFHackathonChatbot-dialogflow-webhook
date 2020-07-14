23e817131dde624956b67e9af60b31b0681e3f21
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-14 20:50:37 +0700

    add basketContext

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ app.post("/webhook", (req, res) => {
     response: res,
   });
 
-  // console.log("agentVersion: " + agent.agentVersion);
-  // console.log("intent: " + agent.intent);
-  // console.log("locale: " + agent.locale);
-  // console.log("query: ", agent.query);
-  // console.log("session: ", agent.session);
+  console.log("agentVersion: " + agent.agentVersion);
+  console.log("intent: " + agent.intent);
+  console.log("locale: " + agent.locale);
+  console.log("query: ", agent.query);
+  console.log("session: ", agent.session);
 
   const itemConfirmYes = (agent) => {
     const item = agent.context.get("item"),
@@ -36,11 +36,32 @@ app.post("/webhook", (req, res) => {
       quantity = item.parameters.quantity,
       type = item.parameters.type;
     console.log("This is agent.context", agent.context);
-    // console.log("This is agent", agent);
-    // console.log("This is item", item);
-    // console.log("This is food", food);
-    // console.log("This is quantity", quantity);
-    // console.log("This is type", type);
+    console.log("This is agent", agent);
+    console.log("This is item", item);
+    console.log("This is food", food);
+    console.log("This is quantity", quantity);
+    console.log("This is type", type);
+
+    var basketContext = {
+        name: "basket",
+        lifespan: 50,
+        parameters: {},
+      },
+      items = {};
+    if (agent.context.get("basket")) {
+      items = agent.context.get("basket").parameters.items;
+    }
+    items[req.body.responseId] = {
+      food: food,
+      quantity: quantity,
+      type: type,
+    };
+    basketContext.parameters.items = items;
+    console.log(JSON.stringify(basketContext));
+    agent.context.set(basketContext);
+    agent.add(
+      `Confirming ${quantity} of ${type} ${food}. Do you want anything else?`
+    );
   };
   const testhook = (agent) => {
     agent.add("The webhook is working!");
